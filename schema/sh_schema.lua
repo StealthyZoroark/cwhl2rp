d4b86f3a93b4e45540183735ffa4aaf8be608d2c
Horns7 <amx710@gmail.com>
2015-07-01 10:40:40 -0700

    sv updates

--- schema/sh_schema.lua
+++ schema/sh_schema.lua
@@ -1,72 +1,152 @@
 --[[
-	� 2012 CloudSixteen.com do not share, re-distribute or modify
+	© 2013 CloudSixteen.com do not share, re-distribute or modify
 	without permission of its author (kurozael@gmail.com).
 --]]
 
-Clockwork.option:SetKey( "default_date", {month = 8, year = 2033, day = 22} );
-Clockwork.option:SetKey( "default_time", {minute = 0, hour = 12, day = 1} );
-Clockwork.option:SetKey("description_business", "Buy and sell bullets for other equipment.");
-Clockwork.option:SetKey("format_singular_cash", "%a");
-Clockwork.option:SetKey("model_shipment", "models/props_junk/cardboard_box003b.mdl");
-Clockwork.option:SetKey("name_business", "Business");
-Clockwork.option:SetKey("intro_image", "newvegas/Metro2033");
-Clockwork.option:SetKey("model_cash", "models/props_lab/box01a.mdl");
-Clockwork.option:SetKey("menu_music", "metro2033.mp3");
-Clockwork.option:SetKey("format_cash", "%a %n");
-Clockwork.option:SetKey("name_cash", "Bullets");
-Clockwork.option:SetKey("gradient", "newvegas/bg_gradient");
-
-Clockwork.attribute:FindByID("Stamina").isOnCharScreen = false;
-
-if (CLIENT) then
-	Clockwork.option:SetColor("information", Color(250, 170, 0, 255));
-	Clockwork.option:SetFont("bar_text", "veg_TargetIDText");
-	Clockwork.option:SetFont("main_text", "veg_MainText");
-	Clockwork.option:SetFont("hints_text", "veg_IntroTextTiny");
-	Clockwork.option:SetFont("large_3d_2d", "veg_Large3D2D");
-	Clockwork.option:SetFont("target_id_text", "veg_TargetIDText");
-	Clockwork.option:SetFont("cinematic_text", "veg_CinematicText");
-	Clockwork.option:SetFont("date_time_text", "veg_IntroTextSmall");
-	Clockwork.option:SetFont("menu_text_big", "veg_MenuTextBig");
-	Clockwork.option:SetFont("menu_text_huge", "veg_MenuTextHuge");
-	Clockwork.option:SetFont("menu_text_tiny", "veg_IntroTextTiny");
-	Clockwork.option:SetFont("intro_text_big", "veg_IntroTextBig");
-	Clockwork.option:SetFont("menu_text_small", "veg_IntroTextSmall");
-	Clockwork.option:SetFont("intro_text_tiny", "veg_IntroTextTiny");
-	Clockwork.option:SetFont("intro_text_small", "veg_IntroTextSmall");
-	Clockwork.option:SetFont("player_info_text", "veg_PlayerInfoText");
-end;
-
--- A function to modify a physical description.
-function Clockwork.kernel:ModifyPhysDesc(description)
-	if ( string.find(description, "|") ) then
-		description = string.gsub(description, "|", "/");
-	end;
-	
-	if (string.len(description) <= 128) then
-		if ( !string.find(string.sub(description, -2), "%p") ) then
-			return description..".";
-		else
-			return description;
-		end;
-	else
-		return string.sub(description, 1, 125).."...";
-	end;
-end;
-
 Clockwork.kernel:IncludePrefixed("cl_schema.lua");
 Clockwork.kernel:IncludePrefixed("cl_hooks.lua");
+Clockwork.kernel:IncludePrefixed("cl_theme.lua");
 Clockwork.kernel:IncludePrefixed("sh_hooks.lua");
 Clockwork.kernel:IncludePrefixed("sv_schema.lua");
 Clockwork.kernel:IncludePrefixed("sv_hooks.lua");
 
-Clockwork.flag:Add("T", "Business", "Access to trade equipment for bullets.");
+Schema.customPermits = {};
 
+for k, v in pairs(_file.Find("models/humans/group17/*.mdl", "GAME")) do
+	Clockwork.animation:AddMaleHumanModel("models/humans/group17/"..v);
+end;
 
-Clockwork.quiz:SetEnabled(true);
+Clockwork.animation:AddCivilProtectionModel("models/eliteghostcp.mdl");
+Clockwork.animation:AddCivilProtectionModel("models/eliteshockcp.mdl");
+Clockwork.animation:AddCivilProtectionModel("models/leet_police2.mdl");
+Clockwork.animation:AddCivilProtectionModel("models/sect_police2.mdl");
+Clockwork.animation:AddCivilProtectionModel("models/policetrench.mdl");
+
+Clockwork.option:SetKey("default_date", {month = 4, year = 2038, day = 1});
+Clockwork.option:SetKey("default_time", {minute = 0, hour = 0, day = 1});
+Clockwork.option:SetKey("format_singular_cash", "%a");
+Clockwork.option:SetKey("model_shipment", "models/items/item_item_crate.mdl");
+Clockwork.option:SetKey("intro_image", "cwmetro2033/metro2033_logo");
+Clockwork.option:SetKey("schema_logo", "cwmetro2033/metro2033_logo");
+Clockwork.option:SetKey("format_cash", "%a %n");
+Clockwork.option:SetKey("menu_music", "avoxgaming/music/armstrong.mp3");
+Clockwork.option:SetKey("name_cash", "Military Grade Rounds");
+Clockwork.option:SetKey("model_cash", "models/props_lab/box01a.mdl");
+Clockwork.option:SetKey("gradient", "halfliferp/dirty");
+
+Clockwork.config:ShareKey("intro_text_small");
+Clockwork.config:ShareKey("intro_text_big");
+Clockwork.config:ShareKey("business_cost");
+Clockwork.config:ShareKey("permits");
+
+Clockwork.quiz:SetEnabled(false);
 Clockwork.quiz:AddQuestion("Do you understand that roleplaying is slow paced and relaxed?", 1, "Yes.", "No.");
 Clockwork.quiz:AddQuestion("Can you type properly, using capital letters and full-stops?", 2, "yes i can", "Yes, I can.");
-Clockwork.quiz:AddQuestion("You do know that this is not DarkRP and you will not get a gun instantly?", 1, "Yes.", "No.");
+Clockwork.quiz:AddQuestion("You do not need weapons to roleplay, do you understand?", 1, "Yes.", "No.");
 Clockwork.quiz:AddQuestion("You do not need items to roleplay, do you understand?", 1, "Yes.", "No.");
 Clockwork.quiz:AddQuestion("What do you think serious roleplaying is about?", 2, "Collecting items and upgrades.", "Developing your character.");
-Clockwork.quiz:AddQuestion("What universe is this roleplaying game set in?", 2, "S.T.A.L.K.E.R.", "Metro 2033");
+Clockwork.quiz:AddQuestion("What universe is this roleplaying game set in?", 2, "Real Life.", "Half-Life 2.");
+
+Clockwork.flag:Add("v", "Light Blackmarket", "Access to light blackmarket goods.");
+Clockwork.flag:Add("V", "Heavy Blackmarket", "Access to heavy blackmarket goods.");
+Clockwork.flag:Add("m", "Resistance Manager", "Access to the resistance manager's goods.");
+
+-- A function to add a custom permit.
+function Schema:AddCustomPermit(name, flag, model)
+	local formattedName = string.gsub(name, "[%s%p]", "");
+	local lowerName = string.lower(name);
+	
+	self.customPermits[ string.lower(formattedName) ] = {
+		model = model,
+		name = name,
+		flag = flag,
+		key = Clockwork.kernel:SetCamelCase(formattedName, true)
+	};
+end;
+
+-- A function to check if a string is a Combine rank.
+function Schema:IsStringCombineRank(text, rank)
+	if (type(rank) == "table") then
+		for k, v in ipairs(rank) do
+			if (self:IsStringCombineRank(text, v)) then
+				return true;
+			end;
+		end;
+	elseif (rank == "EpU") then
+		if (string.find(text, "%pSeC%p") or string.find(text, "%pDvL%p")
+		or string.find(text, "%pEpU%p")) then
+			return true;
+		end;
+	else
+		return string.find(text, "%p"..rank.."%p");
+	end;
+end;
+
+-- A function to check if a player is a Combine rank.
+function Schema:IsPlayerCombineRank(player, rank, realRank)
+	local name = player:Name();
+	local faction = player:GetFaction();
+	
+	if (self:IsCombineFaction(faction)) then
+		if (type(rank) == "table") then
+			for k, v in ipairs(rank) do
+				if (self:IsPlayerCombineRank(player, v, realRank)) then
+					return true;
+				end;
+			end;
+		elseif (rank == "EpU" and !realRank) then
+			if (string.find(name, "%pSeC%p") or string.find(name, "%pDvL%p")
+			or string.find(name, "%pEpU%p")) then
+				return true;
+			end;
+		else
+			return string.find(name, "%p"..rank.."%p");
+		end;
+	end;
+end;
+
+-- A function to get a player's Combine rank.
+function Schema:GetPlayerCombineRank(player)
+	local faction = player:GetFaction();
+	
+	if (faction == FACTION_OTA) then
+		if (self:IsPlayerCombineRank(player, "OWS")) then
+			return 0;
+		elseif (self:IsPlayerCombineRank(player, "EOW")) then
+			return 1;
+		else
+			return 2;
+		end;
+	elseif (self:IsPlayerCombineRank(player, "RCT")) then
+		return 0;
+	elseif (self:IsPlayerCombineRank(player, "04")) then
+		return 1;
+	elseif (self:IsPlayerCombineRank(player, "03")) then
+		return 2;
+	elseif (self:IsPlayerCombineRank(player, "02")) then
+		return 3;
+	elseif (self:IsPlayerCombineRank(player, "01")) then
+		return 4;
+	elseif (self:IsPlayerCombineRank(player, "OfC")) then
+		return 6;
+	elseif (self:IsPlayerCombineRank(player, "EpU", true)) then
+		return 7;
+	elseif (self:IsPlayerCombineRank(player, "DvL")) then
+		return 8;
+	elseif (self:IsPlayerCombineRank(player, "SeC")) then
+		return 9;
+	elseif (self:IsPlayerCombineRank(player, "SCN")) then
+		if (!self:IsPlayerCombineRank(player, "SYNTH")) then
+			return 10;
+		else
+			return 11;
+		end;
+	else
+		return 5;
+	end;
+end;
+
+-- A function to get if a faction is Combine.
+function Schema:IsCombineFaction(faction)
+	return (faction == FACTION_MPF or faction == FACTION_OTA);
+end;
